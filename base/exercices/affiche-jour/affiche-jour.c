6f8e53433fb04b7a09266bd5db7d8ed13f0eb3f7
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-04-05 08:08:46 +0200

    affiche-jour

--- base/exercices/affiche-jour/affiche-jour.c
+++ base/exercices/affiche-jour/affiche-jour.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+enum jours { LUNDI = 1, MARDI, MERCREDI, JEUDI, VENDREDI, SAMEDI, DIMANCHE };
+
 int main(int argc, char **argv)
 {
     /* Affiche un message d'erreur si l'utilisateur oublie l'argument
@@ -13,6 +15,26 @@ int main(int argc, char **argv)
 
     /* Convertit l'argument passé par l'utilisateur en entier. */
     int32_t nombre = atoi(argv[1]);
+
+    switch (nombre)
+    {
+    case LUNDI:
+    case MARDI:
+    case MERCREDI:
+    case JEUDI:
+    case VENDREDI:
+        printf("Le nombre entré correspond à un jour de semaine.\n");
+        break;
+
+    case SAMEDI:
+    case DIMANCHE:
+        printf("Le nombre entré correspond à un jour du week-end.\n");
+        break;
+
+    default:
+        printf("Le nombre entré doit être compris entre 1 et 7!\n");
+        break;
+    }
 
     return EXIT_SUCCESS;
 }
